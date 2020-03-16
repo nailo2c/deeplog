34326dd2b21e8e0dae604d2cc044a1315f9dd4a7
nailo2c <nailo2c@gmail.com>
2020-03-16 16:13:09 +0800

    update predict result

--- README.md
+++ README.md
@@ -15,3 +15,23 @@ Then we use open data from logpai's loghub
 ```python
 python preprocess.py
 ```
+
+## Train
+
+```python
+python train.py --num-class 1143 --num-candidates 114 --epochs 35 --window-size 3
+```
+
+## Predict
+
+```python
+python predict.py
+```
+
+## Result
+
+| Accuracy  | 0.90404 |
+|-----------|---------|
+| Precision | 0.51054 |
+| Recall    | 0.87681 |
+| F1        | 0.64533 |
--- predict.py
+++ predict.py
@@ -5,7 +5,8 @@ import torch
 import logging
 from train import Model
 
-logging.basicConfig(level=logging.WARN,
+
+logging.basicConfig(level=logging.WARNING,
                     format='[%(asctime)s][%(levelname)s]: %(message)s')
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -35,7 +36,7 @@ def model_fn(model_dir):
 
 
 def input_fn(request_body, request_content_type):
-    logger.debug('Deserializing the input data.')
+    # logger.debug('Deserializing the input data.')
     if request_content_type == 'application/json':
         input_data = json.loads(request_body)
         return input_data
@@ -44,20 +45,20 @@ def input_fn(request_body, request_content_type):
 
 
 def predict_fn(input_data, model_info):
-    logger.info('Predict next template on this pattern series.')
+    # logger.info('Predict next template on this pattern series.')
     line = input_data['line']
     num_candidates = model_info['num_candidates']
     input_size = model_info['input_size']
     window_size = model_info['window_size']
     model = model_info['model']
 
-    logger.debug(f'line: {line}')
-    logger.debug(f'num_candidates: {num_candidates}')
-    logger.debug(f'input_size: {input_size}')
-    logger.debug(f'window_size: {window_size}')
+    # logger.debug(f'line: {line}')
+    # logger.debug(f'num_candidates: {num_candidates}')
+    # logger.debug(f'input_size: {input_size}')
+    # logger.debug(f'window_size: {window_size}')
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    logger.debug('Current device: {}'.format(device))
+    # logger.debug('Current device: {}'.format(device))
 
     predict_cnt = 0
     anomaly_cnt = 0
@@ -77,15 +78,81 @@ def predict_fn(input_data, model_info):
 
 
 if __name__ == '__main__':
+    ##############
+    # Load Model #
+    ##############
     model_dir = './model'
     model_info = model_fn(model_dir)
 
+    ###########
+    # predict #
+    ###########
     test_abnormal_list = []
     with open('test_abnormal', 'r') as f:
         for line in f.readlines():
             line = list(map(lambda n: n - 1, map(int, line.strip().split())))
-            print(f'line: {line}')
             request = json.dumps({'line': line})
             input_data = input_fn(request, 'application/json')
             response = predict_fn(input_data, model_info)
             test_abnormal_list.append(response)
+
+    test_normal_list = []
+    with open('test_normal', 'r') as f:
+        for line in f.readlines():
+            line = list(map(lambda n: n - 1, map(int, line.strip().split())))
+            request = json.dumps({'line': line})
+            input_data = input_fn(request, 'application/json')
+            response = predict_fn(input_data, model_info)
+            test_normal_list.append(response)
+
+    ##############
+    # Evaluation #
+    ##############
+    thres = 22
+    abnormal_has_anomaly = [1 if t['anomaly_cnt'] > thres else 0 for t in test_abnormal_list]
+    abnormal_cnt_anomaly = [t['anomaly_cnt'] for t in test_abnormal_list]
+    abnormal_predict = []
+    for test_abnormal in test_abnormal_list:
+        abnormal_predict += test_abnormal['predict_list']
+
+    normal_has_anomaly = [1 if t['anomaly_cnt'] > thres else 0 for t in test_normal_list]
+    normal_cnt_anomaly = [t['anomaly_cnt'] for t in test_normal_list]
+    normal_predict = []
+    for test_normal in test_normal_list:
+        normal_predict += test_normal['predict_list']
+
+    ground_truth = [1]*len(abnormal_has_anomaly) + [0]*len(normal_has_anomaly)
+    predict = abnormal_has_anomaly + normal_has_anomaly
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+    accu = 0
+    for p, t in zip(predict, ground_truth):
+        if p == t:
+            accu += 1
+
+        if p==1 and t==1:
+            TP += 1
+        elif p==1 and t==0:
+            FP += 1
+        elif p==0 and t==1:
+            FN += 1
+        else:
+            TN +=1
+
+    print(f'thres: {thres}')
+    print(f'TP: {TP}')
+    print(f'FP: {FP}')
+    print(f'FP: {TN}')
+    print(f'FP: {FN}')
+
+    accuracy = accu / len(predict)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    F1 = 2 * precision * recall / (precision + recall)
+
+    print(f'accuracy: {accuracy}')
+    print(f'Precision: {precision}')
+    print(f'Recall: {recall}')
+    print(f'F1: {F1}')
